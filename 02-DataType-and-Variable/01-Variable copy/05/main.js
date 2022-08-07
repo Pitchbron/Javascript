55675cd8b7fdca1e2541b81dc7f8f5aa9d45a3d2
Pitchbron <109780446+Pitchbron@users.noreply.github.com>
2022-08-08 00:31:42 +0700

    Add files via upload

---
+++ 02-DataType-and-Variable/01-Variable copy/05/main.js
@@ -0,0 +1,6 @@
+let firstName = "pitchayut";
+let lastName = "Pataraviriyakit";
+let Age = 25;
+let Address = "99/6"
+let display = `Full Name: ${firstName} ${lastName}, Age: ${Age}, Address: ${Address}`
+console.log(display)
---
+++ 02-DataType-and-Variable/02-Operation-With-Variable/02/main.js
@@ -0,0 +1,5 @@
+let a = 1; 
+let b = 2;
+let c = a++; /*c=1*/
+let d = ++c; /*c=2*/
+let e = ++d + d++ + d; /*3+3+4*/
